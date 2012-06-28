e38ef9aad5c0a3560c584b4a379af02354a4b3b1
josh <joshbourdeau@gmail.com>
2012-06-28 11:44:29 -0700

    trying to get heroku to work

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -35,7 +35,7 @@ SampleApp::Application.configure do
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
   
-    # Speed up tests by lowering BCrypt's cost function.
+  # Speed up tests by lowering BCrypt's cost function.
   require 'bcrypt'
   silence_warnings do
     BCrypt::Engine::DEFAULT_COST = BCrypt::Engine::MIN_COST
